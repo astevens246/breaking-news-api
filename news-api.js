75be5f3fb75e713136ef18c8448e4741d588fb97
Allen Stevens <allen.stevens@students.dominican.edu>
2024-07-02 16:23:11 -0700

    added search capability

--- news-api.js
+++ news-api.js
@@ -1,14 +1,9 @@
-export function getNews(query = 'tesla', from_date = '2024-06-02', sortBy = 'publishedAt', page = 1, success, error) {
+export async function getNews(query, from_date, sortBy, page) {
     const apiKey = 'caa94f99e23d40d29d1db0c88389230e';
-    const url = `https://newsapi.org/v2/everything?q=${query}&from=${from_date}&sortBy=${sortBy}&apiKey=${apiKey}&page=${page}`;
+    const url = `https://newsapi.org/v2/everything?q=${query}&from=${from_date}&sortBy=${sortBy}&apiKey=${apiKey}&page=${page}&language=en`;
 
-        fetch(url).then((res) => {
-            return res.json()
-        }).then((json) => {
-            console.log(json)
-        console.log(json.articles)
-        success(json)
-    }).catch((err) => {
-        error(err)
-    })
+        const res = await fetch(url)
+        const json = await res.json()
+
+        return json
 }
