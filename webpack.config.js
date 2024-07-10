9d2a4ad2511523969d854502c1caad44de0f6db7
Allen Stevens <allen.stevens@students.dominican.edu>
2024-07-10 07:39:34 -0700

    added bundling configs

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+// This file is used to configure webpack to build the library
+  const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+		library: 'lib',
+		libraryTarget: 'umd',
+		globalObject: 'this',
+  },
+};
